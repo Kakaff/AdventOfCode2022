b00342629806d3d5a7529810a9b35afb85ad1b25
Jonas Svedberg <Kakaffstudios@gmail.com>
2022-12-02 14:20:17 +0100

    Add project files.

---
+++ Day1_CalorieCounting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+string[][] groupedCalories;
+string inputText;
+
+using (var file = File.OpenRead("./input.txt"))
+    using (var streamReader = new StreamReader(file))
+        inputText = streamReader.ReadToEnd();
+
+groupedCalories = Regex.Matches(inputText, @"((?:[0-9]+(?:\r\n|\r|\n|$))+)")
+    .Where(x => x.Success)
+    .Select(x => x.Value.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+    .ToArray();
+
+var maxCalories = groupedCalories.Max(x => x.Sum(y => int.Parse(y)));
+
+Console.WriteLine($"The elf carrying the most calories is carrying {maxCalories}");
