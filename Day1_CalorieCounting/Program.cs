70755fc3d91cecc22a2285fe699af229989018a1
Jonas Svedberg <Kakaffstudios@gmail.com>
2022-12-02 14:59:34 +0100

    Updated day1 to solve part 2

--- Day1_CalorieCounting/Program.cs
+++ Day1_CalorieCounting/Program.cs
@@ -6,13 +6,13 @@ using (var file = File.OpenRead("./input.txt"))
 using (var streamReader = new StreamReader(file))
     inputText = streamReader.ReadToEnd();
 
-var groupedCalories = Regex.Matches(inputText, @"((?:[0-9]+(?:\r\n|\r|\n|$))+)")
+var caloriesPerElf = Regex.Matches(inputText, @"((?:[0-9]+(?:\r\n|\r|\n|$))+)")
     .Where(x => x.Success)
     .Select(x => x.Value.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
-    .ToArray();
+    .Select(x => x.Sum(y => int.Parse(y)))
+    .OrderDescending();
 
-var maxCalories = groupedCalories.Max(x => x.Sum(y => int.Parse(y)));
-
-Console.WriteLine($"The elf carrying the most calories is carrying {maxCalories}");
+Console.WriteLine($"The elf carrying the most calories is carrying {caloriesPerElf.First()}");
+Console.WriteLine($"The top 3 elves are carrying a total of {caloriesPerElf.Take(3).Sum()}");
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
