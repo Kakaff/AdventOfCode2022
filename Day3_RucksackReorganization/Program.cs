c13376ae219e6b697e008f3898b50c6c34196e88
Jonas Svedberg <uj.svedberg@gmail.com>
2022-12-03 22:33:30 +0100

    another minor change to day 3

--- Day3_RucksackReorganization/Program.cs
+++ Day3_RucksackReorganization/Program.cs
@@ -6,19 +6,18 @@ using (var streamReader = new StreamReader(file))
 
 var rucksacks = inputText.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
 
-var part1DuplicatedTypesPerRucksack = rucksacks.Select(x => (FirstCompartment: x[..(x.Length / 2)], SecondCompartment: x[(x.Length / 2)..]))
+var part1DuplicatedTypesSum = rucksacks.Select(x => (FirstCompartment: x[..(x.Length / 2)], SecondCompartment: x[(x.Length / 2)..]))
                      .Select(x => x.FirstCompartment.Intersect(x.SecondCompartment).Distinct())
-                     .Select(x => x.Select(p => GetItemPriority(p)));
+                     .Select(x => x.Select(p => GetItemPriority(p)))
+                     .Sum(x => x.Sum());
 
-var part2GroupBadges = rucksacks.Cast<IEnumerable<char>>().Chunk(3)
-    .Select(x => x.Aggregate(x.First(), (a,b) => a.Intersect(b)))
-    .Select(x => x.Distinct().Single());
+var part2GroupBadgesSum = rucksacks.Cast<IEnumerable<char>>().Chunk(3)
+    .Select(x => x.Aggregate(x.First(), (a, b) => a.Intersect(b)))
+    .Select(x => GetItemPriority(x.Distinct().Single()))
+    .Sum();
 
-var sumPart1 = part1DuplicatedTypesPerRucksack.Sum(x => x.Sum());
-var sumPart2 = part2GroupBadges.Sum(x => GetItemPriority(x));
-
-Console.WriteLine($"Part1: The sum of the duplicated itemtypes is {sumPart1}");
-Console.WriteLine($"Part2: The sum of the group badges is {sumPart2}");
+Console.WriteLine($"Part1: The sum of the duplicated itemtypes is {part1DuplicatedTypesSum}");
+Console.WriteLine($"Part2: The sum of the group badges is {part2GroupBadgesSum}");
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
 
