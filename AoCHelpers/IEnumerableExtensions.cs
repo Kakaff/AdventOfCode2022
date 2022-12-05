400b671c0188bec1237a465db5131364388e3c01
Jonas Svedberg <uj.svedberg@gmail.com>
2022-12-05 18:24:21 +0100

    Removed commented code

--- AoCHelpers/IEnumerableExtensions.cs
+++ AoCHelpers/IEnumerableExtensions.cs
@@ -8,36 +8,6 @@ namespace AoCHelpers
 {
     public static class IEnumerableExtensions
     {
-        //public static IEnumerable<T?> ForEach<T>(this IEnumerable<T?> enumerable, Action<T?,int> action)
-        //{
-        //    int index = 0;
-        //    var current = default(T);
 
-        //    foreach (var item in enumerable)
-        //    {
-        //        current = enumerable.ElementAt(index);
-        //        action(current, index);
-        //        index++;
-
-        //        yield return current;
-        //    }
-
-        //    yield return current;
-        //}
-
-        //public static IEnumerable<T?> ForEach<T>(this IEnumerable<T?> enumerable, Action<T?> action)
-        //{
-        //    int index = 0;
-        //    T? current;
-
-        //    foreach (var item in enumerable)
-        //    {
-        //        current = enumerable.ElementAt(index);
-        //        action(current);
-        //        index++;
-
-        //        yield return current;
-        //    }
-        //}
     }
 }
