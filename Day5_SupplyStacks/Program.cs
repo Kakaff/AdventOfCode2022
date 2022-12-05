fabf4d492836eb89af86b97aa0ca11f046361967
Jonas Svedberg <Kakaffstudios@gmail.com>
2022-12-05 11:00:18 +0100

    Added solution to day 5 puzzle

---
+++ Day5_SupplyStacks/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.RegularExpressions;
+
+string inputText;
+
+using (var file = File.OpenRead("./input.txt"))
+using (var streamReader = new StreamReader(file))
+    inputText = streamReader.ReadToEnd();
+
+var input = inputText.Split($"{Environment.NewLine}{Environment.NewLine}", StringSplitOptions.RemoveEmptyEntries);
+
+var stackInputRows = input.First()
+    .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => x.Chunk(4).Select((x, index) => (Value: x.Skip(1).First(), StackIndex: index)))
+    .SkipLast(1);
+
+var part1Stacks = new Stack<char>[stackInputRows.First().Count()];
+var part2Stacks = new Stack<char>[part1Stacks.Length];
+
+for (int i = 0; i < part1Stacks.Length; i++)
+{
+    part1Stacks[i] = new Stack<char>();
+    part2Stacks[i] = new Stack<char>();
+}
+
+foreach (var crate in stackInputRows.Reverse().SelectMany(row => row.Where(column => column.Value != ' ')))
+{
+    part1Stacks[crate.StackIndex].Push(crate.Value);
+    part2Stacks[crate.StackIndex].Push(crate.Value);
+}
+
+var commandRows = input.Last();
+
+var commands = Regex.Matches(commandRows, @"move ([0-9]+) from ([0-9]+) to ([0-9]+)", RegexOptions.IgnoreCase)
+    .Select(x => x.Groups.Values.Skip(1).Select(x => int.Parse(x.Value)))
+    .Select(x => (Quantity: x.First(), From: x.Skip(1).First(), To: x.Skip(2).First()));
+
+foreach (var command in commands)
+{
+    var fromPart1 = part1Stacks[command.From - 1];
+    var toPart1 = part1Stacks[command.To - 1];
+
+    var fromPart2 = part2Stacks[command.From - 1];
+    var toPart2 = part2Stacks[command.To - 1];
+
+    var part2CratesToMove = new List<char>();
+
+    for (int i = 0; i < command.Quantity; i++)
+    {
+        toPart1.Push(fromPart1.Pop());
+        part2CratesToMove.Add(fromPart2.Pop());
+    }
+
+    part2CratesToMove.Reverse();
+    part2CratesToMove.ForEach(toPart2.Push);
+
+}
+
+var part1TopMessage = string.Concat(part1Stacks.Select(x => x.Peek()));
+var part2TopMessage = string.Concat(part2Stacks.Select(x => x.Peek()));
+
+Console.WriteLine($"Part 1: The characters at the top of each stack are {part1TopMessage}");
+Console.WriteLine($"Part 2: The characters at the top of each stack are {part2TopMessage}");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
