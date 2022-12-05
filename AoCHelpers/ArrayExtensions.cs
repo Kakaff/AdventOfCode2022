f240ef22b510987cc20f63bd31fa43fe7a8cb0c9
Jonas Svedberg <uj.svedberg@gmail.com>
2022-12-05 18:16:13 +0100

    Added helper library

---
+++ AoCHelpers/ArrayExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCHelpers
+{
+    public static class ArrayExtensions
+    {
+        public static T[] Fill<T>(this T[] array, Func<T> sad)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = sad();
+            }
+
+            return array;
+        }
+    }
+}
