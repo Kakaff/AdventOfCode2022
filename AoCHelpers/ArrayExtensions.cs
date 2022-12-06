00ca6134089edbbb27cc3561c0d1fbb635a9d3b3
Jonas Svedberg <Kakaffstudios@gmail.com>
2022-12-06 09:40:26 +0100

    Added 2d array fill extension and ienumerable foreach

--- AoCHelpers/ArrayExtensions.cs
+++ AoCHelpers/ArrayExtensions.cs
@@ -21,5 +21,37 @@
 
             return array;
         }
+
+        public static T[,] Fill<T>(this T[,] array, Func<T> func)
+        {
+            var rows = array.GetLength(0);
+            var columns = array.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    array[i, j] = func();
+                }
+            }
+
+            return array;
+        }
+
+        public static T[,] Fill<T>(this T[,] array, Func<int,int,T> func)
+        {
+            var rows = array.GetLength(0);
+            var columns = array.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    array[i,j] = func(i, j);
+                }
+            }
+
+            return array;
+        }
     }
 }
--- AoCHelpers/IEnumerableExtensions.cs
+++ AoCHelpers/IEnumerableExtensions.cs
@@ -15,6 +15,21 @@ namespace AoCHelpers
                 yield return enumerable;
         }
 
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var value in enumerable)
+                action(value);
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T,int> action)
+        {
+            int index = 0;
+            foreach (var value in enumerable) {
+                action(value, index);
+                ++index;
+            }
+        }
+
         public static IEnumerable<Tuple<T>> TupleChunk<T>(this IEnumerable<T> enumerable)
         {
             return enumerable.Select(x => Tuple.Create<T>(x));
