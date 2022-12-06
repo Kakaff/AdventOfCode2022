80bd2eea1869ffb43a79e137b18ba6ba12550383
Jonas Svedberg <Kakaffstudios@gmail.com>
2022-12-06 01:16:13 +0100

    removed commands variable in day 5 solution

--- Day5_SupplyStacks/Program.cs
+++ Day5_SupplyStacks/Program.cs
@@ -21,16 +21,15 @@ stackInputRows.Reverse()
         part2Stacks[crate.StackIndex].Push(crate.Value);
     });
 
-var commands = Regex.Matches(input.Last(), @"move ([0-9]+) from ([0-9]+) to ([0-9]+)", RegexOptions.IgnoreCase)
+Regex.Matches(input.Last(), @"move ([0-9]+) from ([0-9]+) to ([0-9]+)", RegexOptions.IgnoreCase)
     .Select(x => x.Groups.Values.Skip(1).Select(x => int.Parse(x.Value)))
     .Select(x => (Quantity: x.First(), From: x.Skip(1).First(), To: x.Skip(2).First()))
-    .ToList();
-
-commands.ForEach(x =>
-{
-    part1Stacks[x.To - 1].PushRange(part1Stacks[x.From - 1].Pop(x.Quantity));
-    part2Stacks[x.To - 1].PushRange(part2Stacks[x.From - 1].Pop(x.Quantity).Reverse());
-});
+    .ToList()
+    .ForEach(x =>
+    {
+        part1Stacks[x.To - 1].PushRange(part1Stacks[x.From - 1].Pop(x.Quantity));
+        part2Stacks[x.To - 1].PushRange(part2Stacks[x.From - 1].Pop(x.Quantity).Reverse());
+    });
 
 Console.WriteLine($"Part 1: The characters at the top of each stack are {string.Concat(part1Stacks.Select(x => x.Peek()))}");
 Console.WriteLine($"Part 2: The characters at the top of each stack are {string.Concat(part2Stacks.Select(x => x.Peek()))}");
