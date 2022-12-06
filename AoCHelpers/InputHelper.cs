4c65e89a589867754e01e8466a16c5376589c6e3
Jonas Svedberg <Kakaffstudios@gmail.com>
2022-12-06 01:04:33 +0100

    Changed InputHelper use System.IO.File read functions

--- AoCHelpers/InputHelper.cs
+++ AoCHelpers/InputHelper.cs
@@ -2,20 +2,7 @@
 {
     public static class InputHelper
     {
-        public static string ReadInputFromFile(string path)
-        {
-            string inputText;
-
-            using (var file = File.OpenRead(path))
-            using (var streamReader = new StreamReader(file))
-                inputText = streamReader.ReadToEnd();
-
-            return inputText;
-        }
-
-        public static string[] ReadInputLinesFromFile(string path)
-        {
-            return ReadInputFromFile(path).Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-        }
+        public static string ReadInputFromFile(string path) => File.ReadAllText(path);
+        public static string[] ReadInputLinesFromFile(string path) => File.ReadAllLines(path);
     }
 }
