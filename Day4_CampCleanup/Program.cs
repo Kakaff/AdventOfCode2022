7a95a3f315b0dbe44d689321ca7d72f50473c35d
Jonas Svedberg <Kakaffstudios@gmail.com>
2022-12-06 02:53:06 +0100

    Refactored Day 4 solution to use new tuple ienumerable extensions

--- Day4_CampCleanup/Program.cs
+++ Day4_CampCleanup/Program.cs
@@ -3,20 +3,14 @@ using AoCHelpers;
 
 var pairs = InputHelper.ReadInputLinesFromFile("./input.txt")
     .Select(pair => pair.Split(',', StringSplitOptions.RemoveEmptyEntries)
-        .Select(range =>
-        {
-            var rangeValues = range.Split('-', StringSplitOptions.RemoveEmptyEntries)
-                                   .Select(num => int.Parse(num));
-
-            return (Start: rangeValues.Min(), End: rangeValues.Max());
-        }))
+        .Select(range => range.Split('-').Select(x => int.Parse(x)).Order().ToTuple<int,int>()))
     .Select(x => (First: x.First(), Second: x.Last()));
 
-var part1FullyOverlappingPairs = pairs.Count(x => (x.First.Start <= x.Second.Start && x.First.End >= x.Second.End)
-                                               || (x.Second.Start <= x.First.Start && x.Second.End >= x.First.End));
+var part1FullyOverlappingPairs = pairs.Count(x => (x.First.Item1 <= x.Second.Item1 && x.First.Item2 >= x.Second.Item2)
+                                                   || (x.Second.Item1 <= x.First.Item1 && x.Second.Item2 >= x.First.Item2));
 
-var part2OverlappingPairs = pairs.Count(x => (x.First.Start <= x.Second.End && x.First.End >= x.Second.Start)
-                                          || (x.Second.Start <= x.First.End && x.Second.End >= x.First.Start));
+    var part2OverlappingPairs = pairs.Count(x => (x.First.Item1 <= x.Second.Item2 && x.First.Item2 >= x.Second.Item1)
+                                              || (x.Second.Item1 <= x.First.Item2 && x.Second.Item2 >= x.First.Item1));
 
 Console.WriteLine($"Part1: The number of fully overlapping pairs is {part1FullyOverlappingPairs}");
 Console.WriteLine($"Part2: The number of overlapping pairs is {part2OverlappingPairs}");
