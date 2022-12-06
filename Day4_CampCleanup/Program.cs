f3d85df4270b42397c5f55f4eb77728b484fd103
Jonas Svedberg <Kakaffstudios@gmail.com>
2022-12-06 02:53:46 +0100

    fixed formatting

--- Day4_CampCleanup/Program.cs
+++ Day4_CampCleanup/Program.cs
@@ -3,14 +3,14 @@ using AoCHelpers;
 
 var pairs = InputHelper.ReadInputLinesFromFile("./input.txt")
     .Select(pair => pair.Split(',', StringSplitOptions.RemoveEmptyEntries)
-        .Select(range => range.Split('-').Select(x => int.Parse(x)).Order().ToTuple<int,int>()))
+        .Select(range => range.Split('-').Select(x => int.Parse(x)).Order().ToTuple<int, int>()))
     .Select(x => (First: x.First(), Second: x.Last()));
 
 var part1FullyOverlappingPairs = pairs.Count(x => (x.First.Item1 <= x.Second.Item1 && x.First.Item2 >= x.Second.Item2)
                                                    || (x.Second.Item1 <= x.First.Item1 && x.Second.Item2 >= x.First.Item2));
 
-    var part2OverlappingPairs = pairs.Count(x => (x.First.Item1 <= x.Second.Item2 && x.First.Item2 >= x.Second.Item1)
-                                              || (x.Second.Item1 <= x.First.Item2 && x.Second.Item2 >= x.First.Item1));
+var part2OverlappingPairs = pairs.Count(x => (x.First.Item1 <= x.Second.Item2 && x.First.Item2 >= x.Second.Item1)
+                                          || (x.Second.Item1 <= x.First.Item2 && x.Second.Item2 >= x.First.Item1));
 
 Console.WriteLine($"Part1: The number of fully overlapping pairs is {part1FullyOverlappingPairs}");
 Console.WriteLine($"Part2: The number of overlapping pairs is {part2OverlappingPairs}");
